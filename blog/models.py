bb5be2139bde9786d7bc7230268b6a855f7c6392
fd <sedent444@gmail.com>
2020-02-25 11:38:34 -0300

    django-p

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+
+
+class post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        sself.save()
+
+    def __str__(self):
+        return self.title
+
+
