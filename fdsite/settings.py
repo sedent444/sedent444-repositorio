c10063f43521e151f07001b6a67ef8f28c35cf3a
fd <sedent444@gmail.com>
2020-02-25 13:53:27 -0300

    commit on master

--- fdsite/settings.py
+++ fdsite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+my_file = os.path.join(THIS_FOLDER, 'myfile.txt')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
